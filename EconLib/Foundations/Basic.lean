c5eafa61607b53c934e39e88f285fa41cc0331c1
Pingju Hsieh <73934073+bing-ru624@users.noreply.github.com>
2026-04-26 17:55:55 +0200

    first version

--- EconLib/Foundations/Basic.lean
+++ EconLib/Foundations/Basic.lean
@@ -1,11 +1,21 @@
 import Mathlib.Analysis.Calculus.Deriv.Basic
 import Mathlib.Analysis.Convex.Function
 
--- 我們定義一個類別，以後只要宣告 [UtilityFunction u]，Lean 就知道它具備這些性質
 class UtilityFunction (u : ℝ → ℝ) : Prop where
   continuous : Continuous u
   increasing : StrictMono u
 
--- 接著封裝風險厭惡（凹性）
+
 class RiskAverse (u : ℝ → ℝ) extends UtilityFunction u where
   concave : ConcaveOn ℝ Set.univ u
+
+-- Example
+def linearUtility (x : ℝ) : ℝ := 2 * x
+
+instance : UtilityFunction linearUtility where
+  continuous := by
+    exact continuous_const.mul continuous_id
+  increasing := by
+    intro x y hxy
+    dsimp [linearUtility]
+    linarith
